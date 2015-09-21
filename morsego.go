114e607d9fc701c937d9b8c27f5c7a7c5bd8c9f2
Prashant Baisla <prashantbaisla@gmail.com>
2015-09-21 14:17:34 +0530

    Generate morse from CLI args and write to specified file

---
+++ morsego.go
@@ -0,0 +1,104 @@
+package main
+
+import (
+  "flag"
+  "fmt"
+  "io"
+  "os"
+  "strings"
+)
+
+func main() {
+
+  outputFile := flag.String("output", "", "output file to write to")
+
+  flag.Parse()
+  writer := openOutputFile(*outputFile)
+
+  if len(flag.Args()) > 0 { // Input from CLI arguments
+
+    morseFromArray(flag.Args(), writer)
+
+  } else { // TODO: Input from file
+  }
+
+}
+
+func openOutputFile(filename string) io.Writer {
+
+  var writer io.Writer
+
+  if filename == "" {
+    writer = os.Stdout
+  } else {
+    var err error
+    writer, err = os.OpenFile(filename, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, os.ModePerm)
+
+    if err != nil {
+      panic(err)
+    }
+  }
+
+  return writer
+}
+
+func morseFromArray(texts []string, writer io.Writer) {
+
+  for _, text := range texts {
+    words := strings.Fields(text)
+
+    for _, word := range words {
+      for _, char := range word {
+
+        morseChar := morse(char)
+        fmt.Fprintf(writer, "%s ", morseChar)
+
+      }
+    }
+  }
+
+}
+
+func morse(letter rune) string {
+  switch string(letter) {
+    case "A", "a": return ".-"
+    case "B", "b": return "-..."
+    case "C", "c": return "-.-."
+    case "D", "d": return "-.."
+    case "E", "e": return "."
+    case "F", "f": return "..--."
+    case "G", "g": return "--."
+    case "H", "h": return "...."
+    case "I", "i": return ".."
+    case "J", "j": return ".---"
+    case "K", "k": return "-.-"
+    case "L", "l": return ".-.."
+    case "M", "m": return "--"
+    case "N", "n": return "-."
+    case "O", "o": return "---"
+    case "P", "p": return ".--."
+    case "Q", "q": return "--.-"
+    case "R", "r": return ".-."
+    case "S", "s": return "..."
+    case "T", "t": return "-"
+    case "U", "u": return "..-"
+    case "V", "v": return "...-"
+    case "W", "w": return ".--"
+    case "X", "x": return "-..-"
+    case "Y", "y": return "-.--"
+    case "Z", "z": return "--.."
+    case "0": return "-----"
+    case "1": return ".----"
+    case "2": return "..---"
+    case "3": return "...--"
+    case "4": return "....-"
+    case "5": return "....."
+    case "6": return "-...."
+    case "7": return "--..."
+    case "8": return "---.."
+    case "9": return "----."
+    case ".": return ".-.-.-"
+    case ",": return "--..--"
+    default: return ""
+  }
+}
